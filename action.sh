29e61d8c5ce20efb0fbbc157818dc161e3cce757
Georgy Shelkovy <rekgrpth@gmail.com>
2025-06-17 08:09:09 +0500

    pipefail

--- action.sh
+++ action.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eux
+set -euxo pipefail
 git clone --progress --single-branch --branch "${INPUTS_BRANCH:-${GITHUB_REF##*/}}" "https://${GITHUB_REPOSITORY_OWNER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" .
